3a05b7342c31a5919acd2d152d035fd6d607e167
HejSidharth <143037077+HejSidharth@users.noreply.github.com>
2023-12-28 01:08:30 -0600

    fixed save button

--- src/pages/subpages/NoteDisplay.jsx
+++ src/pages/subpages/NoteDisplay.jsx
@@ -41,7 +41,7 @@ export default function NoteDisplay() {
     <Sidebar />
     <div className="sm:p-5 p-4 sm:ml-64">
             <div className="mx-auto max-w-screen-xl px-4 lg:px-12 p-2">
-                <button onClick={() => debouncedUpdate(id, title, content)}> <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                <button onClick={() => debouncedUpdate(id, title, content)} className='btn btn-md'> <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
   </svg>Save</button>
               <input className="text-5xl font-bold mx-auto mb-4 bg-base-100 rounded-lg focus:ring-0 outline-none placeholder:text-5xl placeholder:font-bold border-none h-16 w-screen"
